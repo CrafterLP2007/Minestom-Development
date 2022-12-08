5532ce26ac52f42809ec5fc8e530678817be8ff6
GitHub Action <action@github.com>
2022-12-08 13:06:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/crafterlp2007/minestomdevelopment/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/crafterlp2007/minestomdevelopment/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.crafterlp2007.minestomdevelopment.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.crafterlp2007.minestomdevelopment.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/crafterlp2007/minestomdevelopment/services/MyProjectService.kt
+++ src/main/kotlin/com/github/crafterlp2007/minestomdevelopment/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.crafterlp2007.minestomdevelopment.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.crafterlp2007.minestomdevelopment.MyBundle
 
 class MyProjectService(project: Project) {
 
